c8773d61810843a9c844eb875342cc736ed20b98
Trusha-P <trusha.parekh88@gmail.com>
2021-02-28 11:29:14 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -2,15 +2,27 @@ import React from 'react';
 import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
+    renderTableData() {
+        let emjson = this.props.emotions; //coming from App.js
+        return Object.keys(emjson).map( em => {
+            return <tr>
+                <td>{em}</td>
+                <td>{emjson[em]}</td>
+            </tr>;
+      })
+   }
+
     render() {
       return (  
         <div>
           {/*You can remove this line and the line below. */}
-          {JSON.stringify(this.props.emotions)}
+          {/*JSON.stringify(this.props.emotions)*/}
           <table className="table table-bordered">
             <tbody>
             {
                 //Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
+                this.renderTableData()
+            
             }
             </tbody>
           </table>
--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -19,6 +19,8 @@ function getNLUInstance(){
     return NaturalLanguageUnderstanding;
 }
 
+NLUinst = getNLUInstance()
+
 const app = new express();
 
 app.use(express.static('client'))
@@ -31,20 +33,90 @@ app.get("/",(req,res)=>{
   });
 
 app.get("/url/emotion", (req,res) => {
+    const analyzeParams = {
+        'url': req.query.url,
+        'features': {
+            'emotion': {}
+        }
+    };
 
-    return res.send({"happy":"90","sad":"10"});
+    NLUinst.analyze(analyzeParams)
+        .then(analysisResults => {
+            emo = analysisResults.result.emotion.document.emotion;
+            console.log(emo);
+            //console.log(JSON.stringify(analysisResults, null, 2));
+            return res.send(emo);
+        })
+        .catch(err => {
+            console.log('error:', err);
+            return res.send("Error Whatever");
+        });
+    //return res.send({"happy":"90","sad":"10"});
 });
 
 app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+    const analyzeParams = {
+        'url': req.query.url,
+        'features': {
+            'sentiment': {}
+        }
+    };
+    
+    NLUinst.analyze(analyzeParams)
+        .then(analysisResults => {
+            sentim = analysisResults.result.sentiment.document.label;
+            console.log(sentim);
+            console.log(JSON.stringify(analysisResults, null, 2));
+            return res.send(sentim);
+        })
+        .catch(err => {
+            console.log('error:', err);
+            return res.send("Error Whatever");
+        });
+    //return res.send("url sentiment for "+req.query.url);
 });
 
 app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+    const analyzeParams = {
+        'text': req.query.text,
+        'features': {
+            'emotion': {}
+        }
+    };
+
+    NLUinst.analyze(analyzeParams)
+        .then(analysisResults => {
+            emo = analysisResults.result.emotion.document.emotion;
+            console.log(emo);
+            //console.log(JSON.stringify(analysisResults, null, 2));
+            return res.send(emo);
+        })
+        .catch(err => {
+            console.log('error:', err);
+            return res.send("Error Whatever");
+        });
+    //return res.send({"happyy":"10","sad":"90"});
 });
 
 app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+    const analyzeParams = {
+        'text': req.query.text,
+        'features': {
+            'sentiment': {}
+        }
+    };
+    
+    NLUinst.analyze(analyzeParams)
+        .then(analysisResults => {
+            sentim = analysisResults.result.sentiment.document.label;
+            console.log(sentim);
+            return res.send(sentim);
+            //console.log(JSON.stringify(analysisResults, null, 2));
+        })
+        .catch(err => {
+            console.log('error:', err);
+            return res.send("Error Whatever");
+        });
 });
 
 let server = app.listen(8080, () => {
